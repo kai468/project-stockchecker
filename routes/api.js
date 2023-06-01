6a5c76d731ea89d848b6ec720c551f1cc61318b8
User <kai.luessow@fau.de>
2023-06-01 16:26:13 +0200

    5th try render.com issue

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const {MongoClient} = require('mongodb');
 const hash = require('crypto').createHash;
 //const { env } = require('node:process');
 const env = process.env;
-import fetch from "node-fetch";
+const fetch = require('node-fetch').fetch;
 
 
 async function mongodb_main(client, ip_hash, symbol, like){
